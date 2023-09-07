82b0b29ca8cfcf93e601d6d138d61e731c8070ff
samuel-soto2 <1461888@senati.pe>
2023-09-07 16:18:24 -0500

    lo que hicimos fue darle funcion para que se pueda mover las funcionalidades

--- login/Form1.cs
+++ login/Form1.cs
@@ -1,4 +1,10 @@
 
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -12,6 +18,11 @@ namespace login
         {
             InitializeComponent();
         }
+        [DllImport("user32.DLL", EntryPoint = "ReleseCapture")]
+        private extern static void RealeseCapture();
+        [DllImport("user32.DLL", EntryPoint = "SendMessage")]
+        private extern static void SendMessage(System.intPtr hwnd, int wmsg, int wparam, int lparam);
+
 
         private void button1_Click(object sender, EventArgs e)
         {
